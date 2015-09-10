e516427b36c8c14c1b1426a30ab23e75fec293e6
Christoph Wolter <christoph.wolter@comron.de>
2015-09-10 20:46:26 +0600

    create dummy events with index

--- qt-example/src/DummyReader.cpp
+++ qt-example/src/DummyReader.cpp
@@ -19,7 +19,7 @@ void DummyReader::timerFired() {
 
 
     if(counter % 3 == 1) {
-        emit eventReceived("dummy event", 0, false, false, "this is an event", QDateTime::currentDateTimeUtc());
+        emit eventReceived(QString("dummy event %1").arg(counter), 0, false, false, "this is an event", QDateTime::currentDateTimeUtc());
     } else if(counter % 3 == 2) {
         emit valueReceived(dpKey, QString("%1").arg(counter));
     } else {
